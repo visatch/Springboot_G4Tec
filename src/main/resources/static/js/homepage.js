c66bdcc67d2df10e05f6095dbe92e55badeb6b29
Visa <ahvisa1@gmail.com>
2020-12-12 00:30:33 -0800

    testing carousel

--- src/main/resources/static/js/homepage.js
+++ src/main/resources/static/js/homepage.js
@@ -4,7 +4,7 @@ $(window).on('load',function () {
 
 $(document).ready(function () {
     homepage()
-    initOwlCarousel()
+    // initOwlCarousel()
     // let owl = $('.custom1')
     // owl.trigger('initialized.owl.carousel').show();
     // // owl.trigger("refresh.owl.carousel");
@@ -33,12 +33,22 @@ function initOwlCarousel()
         nav: false,
         dots: true,
         loop: true,
-        autoplayTimeout: 4000,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
-        margin: 10,
-        stagePadding: 10,
-        smartSpeed: 400,
+        autoplayTimeout: 2000
+        // autoHeight: true,
+        // lazyLoad: true,
+        // items: 1,
+        // autoplay: true,
+        // nav: false,
+        // dots: true,
+        // loop: true,
+        // autoplayTimeout: 4000,
+        // animateOut: 'fadeOut',
+        // animateIn: 'fadeIn',
+        // margin: 10,
+        // stagePadding: 10,
+        // smartSpeed: 400,
     });
 }
 
